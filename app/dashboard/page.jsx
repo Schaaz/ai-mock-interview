7278a026c52325e6451e5bdd6305076d303e69fa
Shaghil Jawed <shaghiljawed76@gmail.com>
2024-08-13 18:59:33 +0530

    authentication added

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+function Dasboard() {
+  return (
+    <div>Dasboard
+
+      <UserButton/>
+    </div>
+  )
+}
+
+export default Dasboard
