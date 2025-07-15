5709ff45467d08154f4448c63dee3de560629769
SJTriedCoding <shaghiljawed76@gmail.com>
2025-07-16 02:05:20 +0400

    backend changes

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   schema: "./utils/schema.js",
   dialect: 'postgresql',
   dbCredentials: {
-    url: 'postgresql://neondb_owner:9xtfGTDn1mQb@ep-silent-hill-a15bqnyx.ap-southeast-1.aws.neon.tech/ai-interview-mock?sslmode=require',
+    url: 'postgresql://neondb_owner:npg_n3JVDSCR1fbi@ep-empty-night-a16xdsp2-pooler.ap-southeast-1.aws.neon.tech/ai-interview?sslmode=require&channel_binding=require',
   }
 };
