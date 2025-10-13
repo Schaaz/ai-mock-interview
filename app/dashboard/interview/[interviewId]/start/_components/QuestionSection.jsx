ae4ae7d9ca31130d2fae8be02c8e44d4df40d00d
Schaaz <schaaz3allme@gmail.com>
2025-10-14 00:33:35 +0530

    Fixed gemini api & question section

--- app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
@@ -20,7 +20,7 @@ if('speechSynthesis' in window){
             ))}
         </div>
             <h2 className='my-5 text-md md:text-lg'>
-                {mockInterviewQuestion[activeQuestionIndex]?.question}
+                {mockInterviewQuestion[activeQuestionIndex]?.Question}
             </h2>
             <Volume2 className='cursor-pointer' onClick={()=>textToSpeach(mockInterviewQuestion[activeQuestionIndex]?.question)}/>
             <div className='border rounded-lg p-5 bg-blue-100 mt-20'>
--- utils/GeminiAIModal.js
+++ utils/GeminiAIModal.js
@@ -1,29 +1,82 @@
-const {
-  GoogleGenerativeAI,
-  HarmCategory,
-  HarmBlockThreshold,
-} = require("@google/generative-ai");
+import {
+  GoogleGenAI,
+} from '@google/genai';
 
-const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
-const genAI = new GoogleGenerativeAI(apiKey);
-
-const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
-});
-
-const generationConfig = {
-  temperature: 1,
-  topP: 0.95,
-  topK: 64,
-  maxOutputTokens: 8192,
-  responseMimeType: "text/plain",
-};
-
-
-  export const chatSession = model.startChat({
-    generationConfig,
+// async function main() {
+  const ai = new GoogleGenAI({
+    apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
   });
+  const config = {
+    thinkingConfig: {
+      thinkingBudget: -1,
+    },
+  };
+  const model = 'gemini-2.5-flash';
+  const contents = [
+    {
+      role: 'user',
+      parts: [
+        {
+          text: `INSERT_INPUT_HERE`,
+        },
+      ],
+    },
+  ];
+  export const chatSession = ai.chats.create({
+    model,
+    config
+  });
+  // const response = await ai.models.generateContentStream({
+  //   model,
+  //   config,
+  //   contents,
+  // });
+  // let fileIndex = 0;
+  // for await (const chunk of response) {
+  //   console.log(chunk.text);
+  // }
+// }
+
+// main();
+
+// const {
+//   GoogleGenerativeAI,
+//   HarmCategory,
+//   HarmBlockThreshold,
+// } = require("@google/generative-ai");
+
+// const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+// const genAI = new GoogleGenerativeAI(apiKey);
+
+// const model = genAI.getGenerativeModel({
+//   model: "gemini-2.5-flash",
+// });
+
+// const generationConfig = {
+//   temperature: 1,
+//   topP: 0.95,
+//   topK: 64,
+//   maxOutputTokens: 8192,
+//   responseMimeType: "text/plain",
+// };
 
 
+//   export const chatSession = model.startChat({
+//     generationConfig,
+//   });
 
 
+// Use 'import' instead of 'require'
+// import { GoogleGenAI } from "@google/generative-ai";
+
+// const ai = new GoogleGenAI({});
+
+// async function main() {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.5-flash",
+//     contents: "Explain how AI works in a few words",
+//   });
+//   console.log(response.text);
+// }
+
+// await main();
